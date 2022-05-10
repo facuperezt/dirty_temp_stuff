Fixed LRP/masking
Added  Pruning plot usw

LRP.py
@@ -159,7 +159,7 @@ class GraphNet:
         W1p = self.W1 + gamma * self.W1.clamp(min=0)  # god damn gamma rule w^- + yw^+
         W2p = self.W2 + gamma * self.W2.clamp(min=0)
         Vp = self.V + gamma * self.V.clamp(min=0)
-        print(len(A))
+        print("Len adj ",len(A))
         X = torch.eye(len(A)) # diagonal matrix --> baisicly only selfloops
         X.requires_grad_(True)
 
@@ -169,7 +169,7 @@ class GraphNet:
         Z = A.transpose(1, 0).matmul(H.matmul(self.W1)) # Adjacency *  (H * W1)
         Zp = A.transpose(1, 0).matmul(H.matmul(W1p)) #
         H = (Zp * (Z / (Zp + 1e-6)).data).clamp(min=0)
-        print(H.shape)
+
 
         if inds is not None: H = H * Mj + (1 - Mj) * (H.data)
         K = H
@@ -188,7 +188,7 @@ class GraphNet:
         Y = H.mean(dim=0)[l]
 
         Y.backward()
-        print(Y.sum(),(X.data * X.grad).sum(),K.sum(),J.sum(),L.sum())
+        print("Shape res",(X.data * X.grad).shape)
         return X.data * X.grad
 
 # Plotting

create_dataset.py
@@ -238,20 +238,10 @@ def samples(data,split):
     res.to_csv("data/samples_smoll", index=False)
 
 
-def validation_lrp(data,split,adj,model_file,activation=True,pruning=False):
+def validation_lrp(x,adj_t,split,adj,gnn,nn,activation=True,pruning=False):
     #Lets find and remove 5 nodes each for pruning and adding based on samples in validation
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    val_src, val_tar = split["valid"]["source_node"], split["valid"]["target_node"]
-    train_src, train_tar = split["train"]["source_node"], split["train"]["target_node"]
-    print(data)
-    gnn = model.GNN()
-    nn = model.NN()
+    val_src, val_tar = split["source_node"], split["target_node"]
 
-    gnn.load_state_dict(torch.load("model/gnn_"+model_file))
-    nn.load_state_dict(torch.load("model/nn_"+model_file))
-    gnn.to(device), nn.to(device), data.to(device)
-
-    print(data)
     walks_all = utils.walks(adj)
     samples = [8, 14, 107, 453]  # [406]
     df = pd.DataFrame()
@@ -282,43 +272,31 @@ def validation_lrp(data,split,adj,model_file,activation=True,pruning=False):
     if activation:
         for i in samples:
             walks = utils_func.find_walks(val_src[i], val_tar[i], walks_all)
-            nodes = set([node for walk in walks for node in walk])
-            nodes.difference_update([val_src[i],val_tar[i]])
-            ref_adj = data.adj_t.to_dense()
-            for node in nodes :
-                ref_adj[val_src[i], node],ref_adj[node, val_src[i]] = 0,0
-                ref_adj[val_tar[i], node],ref_adj[node, val_tar[i]] = 0,0
-            ref_adj = torch_sparse.SparseTensor.from_dense(ref_adj)
-            graph_rep = gnn(data.x, ref_adj)
+
+            graph_rep = gnn(x, adj_t)
             model_out = nn(graph_rep[val_src], graph_rep[val_tar])
-            r_src, r_tar = nn.lrp(graph_rep[val_src[i]], graph_rep[val_tar[i]], model_out[i])
-            ref = r_src.detach().sum()+r_tar.detach().sum()
-
-            l = []
-            res=[]
-            print("test")
-            while nodes:
+            res = [None]*5
+            print(val_src[i].dtype,val_tar[i],model_out[i])
+            while walks:
+                r = []
+                ref = 0
+                tmp  = None
+                r_src, r_tar = nn.lrp(val_src[i],val_tar[i],model_out[i])
+                nodes = set(np.array(walks).flatten())
+                for walk in walks:
+                    r.append(gnn.lrp(x,adj_t,walk,r_src,r_tar,val_tar[i]))
                 for node in nodes:
-                    tmp = copy.copy(ref_adj)
-                    tmp = tmp.to_dense()
-                    tmp[val_src[i], node], tmp[node, val_src[i]] = data.adj_t.to_dense()[val_src[i], node], data.adj_t.to_dense()[node,val_src[i]]
-                    tmp[val_tar[i], node], tmp[node, val_tar[i]] = data.adj_t.to_dense()[val_tar[i], node], data.adj_t.to_dense()[node,val_tar[i]]
+                    index = np.array([i for i in range(len(walks)) if node in walks[i]])
+                    if r[index].sum() > ref:
+                        tmp= node
 
-                    graph_rep = gnn(data.x, ref_adj)
-                    r_src, r_tar = nn.lrp(graph_rep[val_src[i]], graph_rep[val_tar[i]], model_out[i])
-                    l.append(tuple((ref+r_tar.detach().sum()+r_src.detach().sum(),node)))
-                print(l)
-                l.sort(key=lambda x: x[0])
-                l = [x[1] for x in l]
+                res.append(tmp)
 
-                nodes.discard(l[0])
-                print(nodes)
-                res.append(l[0])
-
-            name = "pruning"+str(i)
+                if res[-1] != None:
+                    break
+            name = "activation"+str(i)
             df[name]=res
             print(df)
-            break
         pass
 
     if activation and pruning:
@@ -327,7 +305,7 @@ def validation_lrp(data,split,adj,model_file,activation=True,pruning=False):
         df = pd.read_csv("data/validation_lrp").to_numpy()
         print(df)
     else :
-        df.to_csv("data/validation_lrp",index=False)
+        df.to_csv("data/validation_lrp_activation",index=False)
         print(df)
 def main():
     """
@@ -348,12 +326,13 @@ def main():
     dataset = dataLoader.LinkPredData("data/", "mini_graph", use_small=True)
     data = dataset.load(transform=False, explain=True)
     edgeindex = data.edge_index
-    data = dataset.load(transform=True, explain=True)
+    data = dataset.load(transform=False, explain=True)
     #graph_statistics(data)
     #graph_split(data)
     #print(test_data)
     #create_Dataset(data.adj_t.to_symmetric().to_dense(),data.x)
     #samples(data,dataset.get_edge_split())
-    validation_lrp(data,dataset.get_edge_split(),utils_func.adjMatrix(edgeindex,data.num_nodes),"2100_50_001")
+    split = dataset.get_edge_split()
+    #validation_lrp(data,split["valid"],utils_func.adjMatrix(edgeindex,data.num_nodes),"2100_50_001")
 if __name__ == "__main__":
     main()

model.py
@@ -7,11 +7,13 @@ from ogb.linkproppred import Evaluator
 from torch.nn.functional import relu
 from torch_geometric.nn import GCNConv
 
+import create_dataset
 import dataLoader
 import plots
 import utils
 import utils_func
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class GNN(torch.nn.Module):
     """
@@ -38,15 +40,16 @@ class GNN(torch.nn.Module):
         def roh(layer):
             with torch.no_grad():
                 cp = copy.deepcopy(layer)
-                cp.lin.weight[:, :] = cp.lin.weight + gamma * torch.clamp(cp.lin.weight, min=0)
+                cp.lin.weight[:, :] = cp.lin.weight + (gamma * torch.clamp(cp.lin.weight, min=0))
                 return cp
 
         A = [None] * 3
-        R = [None] * 3
+        R = [None] * 4
 
-        A[0] = x
+        x.requires_grad_(True)
+        #A[0] = x
 
-        A[0] = relu(A[0]).data.clone().requires_grad_(True)
+        A[0] = x.data.clone().requires_grad_(True)
         A[1] = relu(self.input(A[0], edge_index)).data.clone().requires_grad_(True)
         A[2] = relu(self.hidden(A[1], edge_index)).data.clone().requires_grad_(True)
 
@@ -54,32 +57,47 @@ class GNN(torch.nn.Module):
             R[-1] = r_tar
         else:
             R[-1] = r_src
+        #print(walk)
 
         z = epsilon + roh(self.output).forward(A[2], edge_index)
+        z = z[walk[3]]
+        s = R[3] / (z + 1e-15)
+        (z * s.data).sum().backward()
+        c = A[2].grad
+        R[2] = A[2].data * c
+        R[2] = R[2][walk[2]]
+
+        z = epsilon + roh(self.hidden).forward(A[1], edge_index)
         z = z[walk[2]]
         s = R[2] / (z + 1e-15)
         (z * s.data).sum().backward()
-        c = A[2].grad
-        R[1] = A[2] * c
+        c = A[1].grad
+        R[1] = A[1].data * c
         R[1] = R[1][walk[1]]
 
-        z = epsilon + roh(self.hidden).forward(A[1], edge_index)
-        z = z[walk[1]]
-        s = R[1] / (z + 1e-15)
-        (z * s.data).sum().backward()
-        c = A[1].grad
-        R[0] = A[1] * c
-        R[0] = R[0][walk[0]]
-        print("gnn out:", R[0].sum())
-
+        """
         z = epsilon + roh(self.input).forward(A[0], edge_index)
-        z = z[walk[1]]
+        z = z[walk[0]]
         s = R[0] / (z + 1e-15)
         (z * s.data).sum().backward()
         c = A[0].grad
-        test = A[0] * c
-        return (R[2].sum().detach(),R[1].sum().detach(),R[0].sum().detach())
+        #test = A[0].data * c
+        """
 
+        z = epsilon + roh(self.input).forward(A[0], edge_index)
+        z = z[walk[1]]
+        s = R[1] / (z + 1e-15)
+        (z * s.data).sum().backward()
+        c = A[0].grad
+        R[0] = A[0].data * c
+        #print((np.where(R[0])))
+        R[0] = R[0][walk[0]]
+
+        #print(walk)
+        #print("     GNN out",R[0].sum(),c.sum())
+
+        #return (R[2].sum().detach(),R[1].sum().detach(),R[0].sum().detach())
+        return R[0]
 class NN(torch.nn.Module):
     """
     3-Layer MLP with 256 input and hidden neurons and 1 output neuron
@@ -111,14 +129,11 @@ class NN(torch.nn.Module):
 
         A = [None] * 3
         R = [None] * 3
-
         R[-1] = r
-
         src = src.data.clone().requires_grad_(True)
         tar = tar.data.clone().requires_grad_(True)
 
-        A[0] = relu(src + tar)
-
+        A[0] = src + tar
         A[0] = A[0].data.clone().requires_grad_(True)
         A[1] = relu(self.input(src + tar)).data.clone().requires_grad_(True)
         A[2] = relu(self.hidden(A[1])).data.clone().requires_grad_(True)
@@ -142,13 +157,11 @@ class NN(torch.nn.Module):
         c = A[0].grad
         test = A[0] * c
         """
-
-        z = epsilon + roh(self.input).forward(relu(src + tar))
+        z = epsilon + roh(self.input).forward(src + tar)
         s = R[0] / (z + 1e-15)
         (z * s.data).sum().backward()
         src_grad = src.grad
         tar_grad = tar.grad
-
         return src * src_grad, tar * tar_grad
 
 
@@ -196,29 +209,39 @@ def train(batchsize, train_set, x, adj, optimizer, gnn, nn):
 
 def explains(test_set, gnn, mlp, adj, x,edge_index):
     src, tar = test_set["source_node"], test_set["target_node"]
-    walks_all = utils.walks(adj)
+    #walks_all = utils.walks(adj)
     # forward passes
     mid = gnn(x, edge_index)  # features, edgeindex
     pos_pred = mlp(mid[src], mid[tar])
-    samples = [8, 14,107,453] #[406]
+
+    samples = [53,47,5,188,105]
+    samples = [53]
     abs_R = 0
+    y = copy.deepcopy(x)
+    z = copy.deepcopy(x)
+    gammas = [0.2,0.5]#,1,20]
+    gammas = [0.0]
     for i in samples:
-        print("End score", pos_pred[i])
-        walks = utils_func.find_walks(src[i], tar[i], walks_all)
-        r_src, r_tar = mlp.lrp(mid[src[i]], mid[tar[i]], pos_pred[i])
-        p = []
-        m=0
-        plots.plt_sum(walks,gnn,r_src,r_tar,tar[i],x, edge_index)
-        for walk in walks:
-#            p.append(gnn.lrp(x, edge_index, walk, r_src, r_tar, tar[i]))
-#            m += utils_func.masking(gnn,mlp,x,src[i],tar[i],edge_index,adj,walk,gamma=0)
-            pass
-        #print(m.sum())
-        #abs_R+=utils_func.plot_explain(p, src[i], tar[i], walks, "pos", i)
-        #utils_func.plt_sum(p,pos_pred[i])
+        e = 0.2
+        for gamma in gammas:
+
+            walks = utils_func.walks(adj,src[i], tar[i])
+            r_src, r_tar = mlp.lrp(mid[src[i]], mid[tar[i]], pos_pred[i],gamma=gamma,epsilon=e)
+            p = []
+            m=0
+            #plots.plt_sum(walks,gnn,r_src,r_tar,tar[i],x, edge_index)
+            for walk in walks:
+                p.append(gnn.lrp(y, edge_index, walk, r_src, r_tar, tar[i],gamma=gamma,epsilon=e))
+                #m += utils_func.masking(gnn,mlp,z,src[i],tar[i],edge_index,adj,walk,gamma=gamma)
+            a, pr = utils_func.validation(walks,p,src[i],pruning=True,activation=False,plot=True)
+            print(a)
+            print(pr)
+            break
         break
-    #utils_func.plot_abs(abs_R,samples)
-    print(abs_R,abs_R/len(src))
+            #abs_R+= plots.plot_explain(p, src[i], tar[i], walks, "pos", i,gamma,x)
+            #utils_func.plt_sum(p,pos_pred[i])
+        #utils_func.plot_abs(abs_R,samples)
+    #print(abs_R,abs_R/len(src))
 
 @torch.no_grad()
 def test(batchsize, data_set, x, adj, evaluator, gnn, nn,accuracy=False):
@@ -337,7 +360,7 @@ def main(batchsize=None, epochs=1, explain=True, save=False, load=True, runs=1, 
                                            ["Valid MRR", "Test MRR", "Trainings Error"], 'Model Error',
                                            file_name="GNN" + "performance.pdf")
                 if explain:
-
+                    #validation(valid_set, gnn, nn, exp_adj, explain_data.x,explain_data.edge_index)
                     #utils_func.grid_e_g(valid_set, gnn, nn, exp_adj, explain_data.x,explain_data.edge_index)
                     explains(valid_set, gnn, nn, exp_adj, explain_data.x,explain_data.edge_index)
                     pass

plots.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import igraph
+import utils
 
 def dreiD():
     df1 = pd.read_csv("data/tmp.csv")
@@ -251,57 +252,80 @@ def plot_explain_nodes(walks,src,tar,oneHopSrc, twoHopSrc, oneHopTar, twoHopTar,
     fig.show()
 
 
-def plot_explain(r, src, tar, walks, pos, node):
+def plot_explain(r, src, tar, walks, pos, node,gamma,data):
     graph = igraph.Graph()
     nodes = list(set(np.asarray(walks).flatten()))
     n = 0
-    """
+    print(nodes)
+
     for node in nodes:
         graph.add_vertices(str(node))
-
+    print(graph)
     x, y = [], []
     for walk in walks:
-        graph.add_edges([(str(walk[0]), str(walk[1])), (str(walk[1]), str(walk[2]))])
+        print(walk)
+        graph.add_edges([(str(walk[0]), str(walk[1])), (str(walk[1]), str(walk[2])),(str(walk[2]), str(walk[3]))])
         x.append(nodes.index(walk[0])), y.append(nodes.index(walk[1]))
         x.append(nodes.index(walk[1])), y.append(nodes.index(walk[2]))
+        x.append(nodes.index(walk[2])), y.append(nodes.index(walk[1]))
+
 
     place = np.array(list(graph.layout_kamada_kawai()))
-
     # edges plotting
 
     fig, axs = plt.subplots()
-    """
     val_abs = 0
+    max_abs = np.abs(max(map((lambda x:x.sum()),r)).detach().numpy())
+
     for walk in walks:
         R = r[n].detach()
 
         R = R.sum()
 
-        """
-        a = [place[nodes.index(walk[0]), 0], place[nodes.index(walk[1]), 0], place[nodes.index(walk[2]), 0]]
-        b = [place[nodes.index(walk[0]), 1], place[nodes.index(walk[1]), 1], place[nodes.index(walk[2]), 1]]
+        a = [place[nodes.index(walk[0]), 0], place[nodes.index(walk[1]), 0], place[nodes.index(walk[2]), 0], place[nodes.index(walk[3]), 0]]
+        b = [place[nodes.index(walk[0]), 1], place[nodes.index(walk[1]), 1], place[nodes.index(walk[2]), 1], place[nodes.index(walk[3]), 1]]
         tx, ty = utils.shrink(a, b)
 
-        #print(walk, "with relevance of ", R)
-        axs.plot([place[x, 0], place[y, 0]], [place[x, 1], place[y, 1]], color='gray', lw=0.2, ls='dotted', alpha=0.3)
+        axs.arrow(a[0], b[0], a[1]-a[0],b[1]-b[0], color='grey', lw=0.5, alpha=0.3,length_includes_head=True,
+                  head_width=0.05)
+        axs.arrow(a[1], b[1], a[2] - a[1], b[2] - b[1], color='grey', lw=0.5, alpha=0.3, length_includes_head=True,
+                  head_width=0.05)
+        axs.arrow(a[2], b[2], a[3] - a[2], b[3] - b[2], color='grey', lw=0.5, alpha=0.3, length_includes_head=True,
+                  head_width=0.05)
 
         if R > 0.0:
-            alpha = np.clip(5 * R.data.numpy(), 0, 1)
+            alpha = np.clip((2/max_abs) * R.data.numpy(), 0, 1)
             axs.plot(tx, ty, alpha=alpha, color='red', lw=1.2)
-            # print("     and alpha of", alpha)
+
         if R < -0.0:
-            alpha = np.clip(-5 * R.data.numpy(), 0, 1)
+            alpha = np.clip(-(2/max_abs) * R.data.numpy(), 0, 1)
             axs.plot(tx, ty, alpha=alpha, color='blue', lw=1.2)
-            # print("     and alpha of", alpha)
-        """
+
         n += 1
 
         val_abs += np.abs(R)
+
     # nodes plotting
-    """
-    axs.plot(place[:, 0], place[:, 1], 'o', color='black', ms=3)
-    axs.plot(place[nodes.index(src), 0], place[nodes.index(src), 1], 'o', color='green', ms=6, label="source node")
-    axs.plot(place[nodes.index(tar), 0], place[nodes.index(tar), 1], 'o', color='yellow', ms=6, label="target node")
+    alpha_src = np.sqrt(((data[src].numpy() - data[nodes].numpy())**2).sum(axis=1))
+    alpha_src *= 1/max(alpha_src)
+
+    alpha_tar = np.sqrt(((data[tar].numpy() - data[nodes].numpy())**2).sum(axis=1))
+    alpha_tar *= 1 / max(alpha_tar)
+    print(alpha_tar)
+    print(alpha_src)
+    for i in range(len(nodes)):
+        """
+        axs.plot(place[i, 0], place[i, 1], 'o', color='green', ms=9,
+                 fillstyle='bottom',alpha=alpha_src[i])
+        axs.plot(place[i, 0], place[i, 1], 'o', color='yellow', ms=9,
+                 fillstyle='top',alpha=alpha_tar[i])
+        """
+        axs.plot(place[i, 0], place[i, 1], 'o', color='black', ms=3)
+
+    axs.plot(place[nodes.index(src), 0], place[nodes.index(src), 1], 'o',
+             color='green', ms=5, label="source node")
+    axs.plot(place[nodes.index(tar), 0], place[nodes.index(tar), 1], 'o',
+             color='yellow', ms=5, label="target node")
 
     # legend shenenigans & # plot specifics
     axs.plot([], [], color='blue', label="negative relevance")
@@ -310,9 +334,24 @@ def plot_explain(r, src, tar, walks, pos, node):
     axs.legend(loc=2, bbox_to_anchor=(-0.15, 1.14))
     axs.axis("off")
 
-    node = str(node)
-    name = "plots/LRP_plot_" + pos + "_example_" + node + ".svg"
+    gamma = str(gamma)
+    gamma = gamma.replace('.','')
+    node = str(src)
+    name = "plots/LRP_plot_" + pos + "_example_" + node +gamma+".svg"
     fig.savefig(name)
     fig.show()
-    """
     return val_abs
+
+def validation(relevances: list,node):
+    relevances = np.asarray(relevances)
+    fig, axs = plt.subplots()
+    axs.fill_between(np.arange(0,25,1),relevances[:,1])
+    axs.set_xticks([0, 5, 10, 15, 20, 25], labels=[0, 5, 10, 15, 20, 25])
+
+
+#    axs.fill_between(np.arange(0,4,1),relevances[:,1])
+#    axs.set_xticks([0, 1, 2, 3 ], labels=[0, 1, 2, 3])
+
+
+    plt.savefig("plots/validation_pru"+str(node))
+    plt.show()

utils_func.py
@@ -6,9 +6,9 @@ import torch
 import copy
 import utils
 import matplotlib.lines as mlines
-
-
+import plots
 def adjMatrix(edges, numNodes, selfLoops=True):
+
     """
     Function to calculate Adjacency MMatrix given an edge Array
     :param edges:  Dimensions 2 x m with the fst of the two being source and the snd being target
@@ -127,53 +127,91 @@ def grid_e_g(test_set, gnn, mlp, adj, x,edge_index):
 
 def masking(gnn,nn,x,src,tar,edge_index,adj,walk,gamma=0):
 
-    def roh(layer,gamma):
-        with torch.no_grad():
-            cp = copy.deepcopy(layer)
-            cp.lin.weight[:, :] = cp.lin.weight + gamma * torch.clamp(cp.lin.weight, min=0)
-            return cp
-
-    def roh_lin(layer,gamma):
-        with torch.no_grad():
-            cp = copy.deepcopy(layer)
-            cp.weight[:, :] = cp.weight + gamma * torch.clamp(cp.weight, min=0)
-            return cp
-
-    torch.autograd.set_detect_anomaly(True)
-    x.requires_grad_(True)
+    M = [None]*4
+    M[0] = torch.FloatTensor(np.eye(x.shape[0])[walk[0]][:, np.newaxis])
+    M[1] = torch.FloatTensor(np.eye(x.shape[0])[walk[1]][:, np.newaxis])
+    M[2] = torch.FloatTensor(np.eye(x.shape[0])[walk[2]][:, np.newaxis])
+    M[3] = torch.FloatTensor(np.eye(x.shape[0])[walk[3]][:, np.newaxis])
     H = [None]*6
 
-    Mj = torch.FloatTensor(np.eye(len(adj))[walk[0]][:, np.newaxis])
-    Mk = torch.FloatTensor(np.eye(len(adj))[walk[1]][:, np.newaxis])
-    Ml = torch.FloatTensor(np.eye(len(adj))[walk[2]][:, np.newaxis])
+    x.requires_grad_(True)
+    x = x*M[0] + (1-M[0])*x.data
+    x.retain_grad()
 
-    Z = roh(gnn.input,gamma=0).forward(x,edge_index)  # Adjacency *  (H * W1)
-    Zp= roh(gnn.input,gamma=gamma).forward(x,edge_index)#
-    H[0] = (Zp * (Z / (Zp + 1e-15)).data).clamp(min=0)
-    H[0] = H[0] * Mj + (1 - Mj) * (H[0].data)
+    H[0] = copy.deepcopy(gnn.input).forward(x,edge_index).clamp(min=0)
+    H[0] = H[0]*M[1] + (1-M[1])*H[0].data
 
-    Z = roh(gnn.hidden, gamma=0).forward(H[0], edge_index)  # Adjacency *  (H * W1)
-    Zp = roh(gnn.hidden, gamma=gamma).forward(H[0], edge_index)  #
-    H[1] = (Zp * (Z / (Zp + 1e-15)).data).clamp(min=0)
-    H[1] = H[1] * Mk + (1 - Mk) * (H[1].data)
+    H[1] = copy.deepcopy(gnn.hidden).forward(H[0], edge_index).clamp(min=0)
+    H[1] = H[1] * M[2] + (1 - M[2]) * H[1].data
 
-    Z = roh(gnn.output, gamma=0).forward(H[1], edge_index)  # Adjacency *  (H * W1)
-    Zp = roh(gnn.output, gamma=gamma).forward(H[1], edge_index)
-    H[2] = (Zp * (Z / (Zp + 1e-15)).data)
-    H[2] = H[2] * Ml + (1 - Ml) * (H[2].data)
+    H[2] = copy.deepcopy(gnn.output).forward(H[1], edge_index)
+    H[2] = H[2] * M[3] + (1 - M[3]) * H[2].data
 
-    x_nn = H[2][src]+H[2][tar]
-    H[3] = roh_lin(nn.input,gamma=0).forward(x_nn).clamp(min=0)
-    H[4] = roh_lin(nn.hidden, gamma=0).forward(H[3]).clamp(min=0)
-
-    #Z = roh_lin(nn.output, gamma=0).forward(H[4])  # Adjacency *  (H * W1)
-    #Zp = roh_lin(nn.output, gamma=gamma).forward(H[4])
-
-    #H[5] = (Zp * (Z / (Zp + 1e-6)).data)
-    #H[5] = H[5] * Ml + (1 - Ml) * (H[5].data)
-    H[5] = roh_lin(nn.output, gamma=0).forward(H[4])
+    x_n = H[2][src]+H[2][tar]
+    H[3] = copy.deepcopy(nn.input).forward(x_n).clamp(min=0)
+    H[4] = copy.deepcopy(nn.hidden).forward(H[3]).clamp(min=0)
+    H[5] = copy.deepcopy(nn.output).forward(H[4])
     H[5].backward()
-
-    print("masking",(x.grad*x.data).sum())
-
+    print("Masking",(x.grad*x.data).sum(),x.grad.sum())
+    print(np.where((x.grad*x.data)))
     return x.grad*x.data
+
+
+def walks(A,src,tar):
+    w = []
+    for v1 in [src,tar]:
+        for v2 in np.where(A[:,v1])[0]:
+            for v3 in np.where(A[:,v2])[0]:
+                for v4 in np.where(A[:,v3])[0]:
+                    w += [[v4, v3, v2,v1.numpy().flatten()[0]]]
+
+    return w
+
+def validation(walks:list,relevance:list,node_src,pruning :bool, activation:bool,plot:bool):
+    nodes = list(set(np.asarray(walks).flatten()))
+    print(len(nodes))
+    res_pruning,res_activation = [],[]
+    if activation:
+        R_g = 0
+        res_activation = []
+        for i in range(25):
+            R_max,old = (0,0),0
+            for node in nodes:
+                s = np.asarray([relevance[x].sum() for x in range(len(walks)) if node in walks[x]])
+                if (s.sum() + R_g) > old :
+                    R_max = (node,s.sum())
+                    old = s.sum() + R_g
+            nodes.remove(R_max[0])
+            res_activation.append((R_max[0],R_max[1]+R_g))
+            R_g += R_max[1]
+        if plot : plots.validation(res_activation,node_src)
+
+    elif pruning:
+        print(nodes)
+        R_g = np.asarray(relevance).sum().sum()
+        print(R_g)
+        res_pruning = []
+        for i in range(25):
+            R_min, old = (0, 0), R_g
+            for node in nodes:
+                s = np.asarray([relevance[x].sum() for x in range(len(walks)) if node not in walks[x]])
+                print("current node",node,np.abs(s.sum() - R_g),s.sum())
+                print("current old", old)
+                if np.abs(s.sum() - R_g) < old:
+                    R_min = (node, s.sum())
+                    print("     Rmin",R_min)
+                    old = np.abs(s.sum() - R_g)
+                    print("     new old",old)
+            print("------------------------------")
+
+            nodes.remove(R_min[0])
+            res_pruning.append((R_min[0],np.abs(R_min[1] - R_g)))
+            print("pre minus", R_g)
+            #R_g -= np.abs(R_min[1] - R_g)
+            print("post minus",R_g)
+
+        print(res_pruning)
+
+        if plot: plots.validation(res_pruning,node_src)
+
+    return res_activation,res_pruning